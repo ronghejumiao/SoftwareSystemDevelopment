1c3c92483cce206e55268cfd20c0eb1c617441e4
20236855 <2848766446@qq.com>
2025-06-21 23:51:20 +0800

    zz

---
+++ RuoYi-Vue-master/ruoyi-ui/src/views/course/index.vue
@@ -0,0 +1,372 @@
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
+      <el-form-item label="课程名称" prop="courseName">
+        <el-input
+          v-model="queryParams.courseName"
+          placeholder="请输入课程名称"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="课程编号，唯一标识" prop="courseCode">
+        <el-input
+          v-model="queryParams.courseCode"
+          placeholder="请输入课程编号，唯一标识"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="课程分类" prop="courseCategory">
+        <el-input
+          v-model="queryParams.courseCategory"
+          placeholder="请输入课程分类"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="授课教师ID，关联sys_user表" prop="teacherId">
+        <el-input
+          v-model="queryParams.teacherId"
+          placeholder="请输入授课教师ID，关联sys_user表"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="课程学分" prop="credit">
+        <el-input
+          v-model="queryParams.credit"
+          placeholder="请输入课程学分"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="课程学时" prop="hours">
+        <el-input
+          v-model="queryParams.hours"
+          placeholder="请输入课程学时"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="课程状态" prop="status">
+        <el-select v-model="queryParams.status" placeholder="请选择课程状态" clearable>
+          <el-option
+            v-for="dict in dict.type.char"
+            :key="dict.value"
+            :label="dict.label"
+            :value="dict.value"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button
+          type="primary"
+          plain
+          icon="el-icon-plus"
+          size="mini"
+          @click="handleAdd"
+          v-hasPermi="['system:course:add']"
+        >新增</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="success"
+          plain
+          icon="el-icon-edit"
+          size="mini"
+          :disabled="single"
+          @click="handleUpdate"
+          v-hasPermi="['system:course:edit']"
+        >修改</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="danger"
+          plain
+          icon="el-icon-delete"
+          size="mini"
+          :disabled="multiple"
+          @click="handleDelete"
+          v-hasPermi="['system:course:remove']"
+        >删除</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="warning"
+          plain
+          icon="el-icon-download"
+          size="mini"
+          @click="handleExport"
+          v-hasPermi="['system:course:export']"
+        >导出</el-button>
+      </el-col>
+      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <el-table v-loading="loading" :data="courseList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column label="课程ID，主键，自增" align="center" prop="courseId" />
+      <el-table-column label="课程名称" align="center" prop="courseName" />
+      <el-table-column label="课程编号，唯一标识" align="center" prop="courseCode" />
+      <el-table-column label="课程分类" align="center" prop="courseCategory" />
+      <el-table-column label="授课教师ID，关联sys_user表" align="center" prop="teacherId" />
+      <el-table-column label="课程学分" align="center" prop="credit" />
+      <el-table-column label="课程学时" align="center" prop="hours" />
+      <el-table-column label="课程描述" align="center" prop="courseDesc" />
+      <el-table-column label="课程状态" align="center" prop="status">
+        <template slot-scope="scope">
+          <dict-tag :options="dict.type.char" :value="scope.row.status"/>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-edit"
+            @click="handleUpdate(scope.row)"
+            v-hasPermi="['system:course:edit']"
+          >修改</el-button>
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-delete"
+            @click="handleDelete(scope.row)"
+            v-hasPermi="['system:course:remove']"
+          >删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="queryParams.pageNum"
+      :limit.sync="queryParams.pageSize"
+      @pagination="getList"
+    />
+
+    <!-- 添加或修改课程信息，存储课程的基本信息对话框 -->
+    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="课程名称" prop="courseName">
+          <el-input v-model="form.courseName" placeholder="请输入课程名称" />
+        </el-form-item>
+        <el-form-item label="课程编号，唯一标识" prop="courseCode">
+          <el-input v-model="form.courseCode" placeholder="请输入课程编号，唯一标识" />
+        </el-form-item>
+        <el-form-item label="课程分类" prop="courseCategory">
+          <el-input v-model="form.courseCategory" placeholder="请输入课程分类" />
+        </el-form-item>
+        <el-form-item label="授课教师ID，关联sys_user表" prop="teacherId">
+          <el-input v-model="form.teacherId" placeholder="请输入授课教师ID，关联sys_user表" />
+        </el-form-item>
+        <el-form-item label="课程学分" prop="credit">
+          <el-input v-model="form.credit" placeholder="请输入课程学分" />
+        </el-form-item>
+        <el-form-item label="课程学时" prop="hours">
+          <el-input v-model="form.hours" placeholder="请输入课程学时" />
+        </el-form-item>
+        <el-form-item label="课程描述" prop="courseDesc">
+          <el-input v-model="form.courseDesc" type="textarea" placeholder="请输入内容" />
+        </el-form-item>
+        <el-form-item label="课程状态" prop="status">
+          <el-select v-model="form.status" placeholder="请选择课程状态">
+            <el-option
+              v-for="dict in dict.type.char"
+              :key="dict.value"
+              :label="dict.label"
+              :value="dict.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+        <el-button @click="cancel">取 消</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { listCourse, getCourse, delCourse, addCourse, updateCourse } from "@/api/system/course"
+
+export default {
+  name: "Course",
+  dicts: ['char'],
+  data() {
+    return {
+      // 遮罩层
+      loading: true,
+      // 选中数组
+      ids: [],
+      // 非单个禁用
+      single: true,
+      // 非多个禁用
+      multiple: true,
+      // 显示搜索条件
+      showSearch: true,
+      // 总条数
+      total: 0,
+      // 课程信息，存储课程的基本信息表格数据
+      courseList: [],
+      // 弹出层标题
+      title: "",
+      // 是否显示弹出层
+      open: false,
+      // 查询参数
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10,
+        courseName: null,
+        courseCode: null,
+        courseCategory: null,
+        teacherId: null,
+        credit: null,
+        hours: null,
+        courseDesc: null,
+        status: null
+      },
+      // 表单参数
+      form: {},
+      // 表单校验
+      rules: {
+        courseName: [
+          { required: true, message: "课程名称不能为空", trigger: "blur" }
+        ],
+        courseCode: [
+          { required: true, message: "课程编号，唯一标识不能为空", trigger: "blur" }
+        ],
+        courseCategory: [
+          { required: true, message: "课程分类不能为空", trigger: "blur" }
+        ],
+        teacherId: [
+          { required: true, message: "授课教师ID，关联sys_user表不能为空", trigger: "blur" }
+        ],
+        credit: [
+          { required: true, message: "课程学分不能为空", trigger: "blur" }
+        ],
+        hours: [
+          { required: true, message: "课程学时不能为空", trigger: "blur" }
+        ],
+        createTime: [
+          { required: true, message: "创建时间不能为空", trigger: "blur" }
+        ],
+      }
+    }
+  },
+  created() {
+    this.getList()
+  },
+  methods: {
+    /** 查询课程信息，存储课程的基本信息列表 */
+    getList() {
+      this.loading = true
+      listCourse(this.queryParams).then(response => {
+        this.courseList = response.rows
+        this.total = response.total
+        this.loading = false
+      })
+    },
+    // 取消按钮
+    cancel() {
+      this.open = false
+      this.reset()
+    },
+    // 表单重置
+    reset() {
+      this.form = {
+        courseId: null,
+        courseName: null,
+        courseCode: null,
+        courseCategory: null,
+        teacherId: null,
+        credit: null,
+        hours: null,
+        courseDesc: null,
+        createTime: null,
+        updateTime: null,
+        status: null
+      }
+      this.resetForm("form")
+    },
+    /** 搜索按钮操作 */
+    handleQuery() {
+      this.queryParams.pageNum = 1
+      this.getList()
+    },
+    /** 重置按钮操作 */
+    resetQuery() {
+      this.resetForm("queryForm")
+      this.handleQuery()
+    },
+    // 多选框选中数据
+    handleSelectionChange(selection) {
+      this.ids = selection.map(item => item.courseId)
+      this.single = selection.length!==1
+      this.multiple = !selection.length
+    },
+    /** 新增按钮操作 */
+    handleAdd() {
+      this.reset()
+      this.open = true
+      this.title = "添加课程信息，存储课程的基本信息"
+    },
+    /** 修改按钮操作 */
+    handleUpdate(row) {
+      this.reset()
+      const courseId = row.courseId || this.ids
+      getCourse(courseId).then(response => {
+        this.form = response.data
+        this.open = true
+        this.title = "修改课程信息，存储课程的基本信息"
+      })
+    },
+    /** 提交按钮 */
+    submitForm() {
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+          if (this.form.courseId != null) {
+            updateCourse(this.form).then(response => {
+              this.$modal.msgSuccess("修改成功")
+              this.open = false
+              this.getList()
+            })
+          } else {
+            addCourse(this.form).then(response => {
+              this.$modal.msgSuccess("新增成功")
+              this.open = false
+              this.getList()
+            })
+          }
+        }
+      })
+    },
+    /** 删除按钮操作 */
+    handleDelete(row) {
+      const courseIds = row.courseId || this.ids
+      this.$modal.confirm('是否确认删除课程信息，存储课程的基本信息编号为"' + courseIds + '"的数据项？').then(function() {
+        return delCourse(courseIds)
+      }).then(() => {
+        this.getList()
+        this.$modal.msgSuccess("删除成功")
+      }).catch(() => {})
+    },
+    /** 导出按钮操作 */
+    handleExport() {
+      this.download('system/course/export', {
+        ...this.queryParams
+      }, `course_${new Date().getTime()}.xlsx`)
+    }
+  }
+}
+</script>
